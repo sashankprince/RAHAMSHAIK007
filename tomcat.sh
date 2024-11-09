9f2da2745e317060e5410c57e05919c2fe7656af
sashankprince <130961071+sashankprince@users.noreply.github.com>
2024-11-09 11:36:24 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,6 +1,6 @@
 amazon-linux-extras install java-openjdk11 -y
 wget https://downloads.apache.org/tomcat/tomcat-10/v10.1.9/bin/apache-tomcat-10.1.9.tar.gz
-tar -zxvf apache-tomcat-9.0.90.tar.gz
+tar -zxvf apache-tomcat-v10.1.9.tar.gz
 sed -i '56 a\<role rolename="manager-gui"/>' apache-tomcat-v10.1.9/conf/tomcat-users.xml
 sed -i '57 a\<role rolename="manager-script"/>' apache-tomcat-v10.1.9/conf/tomcat-users.xml
 sed -i '58 a\<user username="tomcat" password="sashank123" roles="manager-gui, manager-script"/>' apache-tomcat-v10.1.9/conf/tomcat-users.xml
