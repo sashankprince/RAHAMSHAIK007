bf28322cb10c9db5b8718739d32e8895aff66bf5
sashankprince <130961071+sashankprince@users.noreply.github.com>
2025-01-02 12:50:31 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket rahamssshaik09.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket rahamssshaik09.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://rahamssshaik09.k8s.local
-kops create cluster --name sashank.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name sashank.k8s.local --yes --admin
+kops create cluster --name sashankprince.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name sashankprince.k8s.local --yes --admin
